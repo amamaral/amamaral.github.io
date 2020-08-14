3c669cce9cb8ca9a4bd8df8fd08df21ba421d702
amamaral <anderson.amaral@outlook.com>
2020-08-14 07:34:46 -0300

    Atualiza apresentacao AVA

--- teaching/Guia_basico_AVA/compila_pandoc.bat
+++ teaching/Guia_basico_AVA/compila_pandoc.bat
@@ -2,7 +2,7 @@ SET filename=apresentacao
 
 
 :: Add --self-contained to see slides without internet connection...
-pandoc -s -i -t revealjs %filename%.pmd -o %filename%.html --mathjax --highlight-style=tango --default-image-extension=svg
+pandoc -s -i -t revealjs %filename%.pmd -o %filename%.html --mathjax --highlight-style=tango --default-image-extension=svg --include-after-body=footer.html
 :: Optional beamer output
 ::pandoc -s -i -t beamer %filename%.pmd -o %filename%.pdf --highlight-style=tango --default-image-extension=pdf
 
